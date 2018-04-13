f5065f839757f70eccd0146d1cb51f928947fbd6
Ben Glasder <bglasder@gmail.com>
2018-04-13 00:51:38 -0500

    added first version of running site

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -6,7 +6,6 @@ class AboutPage extends React.Component {
       <div className="jumbotron">
         <h1>About</h1>
         <p>This application uses React, Redux, React Router and a variety of other helpful libraries.</p>
-        <Link to="about" className="btn btn-primary btn-lg">Learn more </Link>
       </div>
     );
   }
