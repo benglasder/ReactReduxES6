4b15c0e6ae84d8c8f448ad2b12c4055d013cb901
Ben Glasder <bglasder@gmail.com>
2018-04-13 03:09:57 -0500

    steppedthroughReduxFlow

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -25,10 +25,16 @@ class CoursesPage extends React.Component {
     this.props.dispatch(courseActions.createCourse(this.state.course));
   }
 
+  courseRow(course, index){
+    return <div key={index}>{course.title}</div>;
+  }
+
   render () {
+    debugger;
     return (
       <div>
         <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
         <h2>Add Course</h2>
         <input type="text" onChange={this.onTitleChange}
         value={this.state.course.title}/>
@@ -41,7 +47,14 @@ class CoursesPage extends React.Component {
   }
 }
 
+CoursesPage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  courses: PropTypes.array.isRequired
+};
+
+
 function mapStateToProps(state, ownProps){
+  debugger;
   return {
     courses: state.courses
   };
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,6 +1,7 @@
 export default function courseReducer(state = [], action) {
   switch(action.type) {
     case 'CREATE_COURSE':
+      debugger;
       return [...state, //returns a spread out new version of the state object we passed in
       Object.assign({},action.course)];
 
