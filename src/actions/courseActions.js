c677267d95ddd0e74a45e5b3fdb4951350159124
Ben Glasder <bglasder@gmail.com>
2018-04-13 10:45:02 -0500

    addedCreateandUpdateActions

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -5,6 +5,14 @@ export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses};
 }
 
+export function createCourseSuccess(course) {
+  return {type: types.CREATE_COURSE_SUCCESS, course};
+}
+
+export function updateCourseSuccess(course) {
+  return {type: types.UPDATE_COURSE_SUCCESS, course};
+}
+
 
 
 export function loadCourses() {
@@ -12,6 +20,17 @@ export function loadCourses() {
     // Fetch call or AJAX call would go here if we were not using a mockApi
     return courseApi.getAllCourses().then(courses => {
       dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
+  };
+}
+
+export function saveCourse(course) {
+  return function (dispatch, getState) {
+    return courseApi.saveCourse(course).then(savedCourse => {
+      course.id ? dispatch(updateCourseSuccess(savedCourse)) :
+        dispatch(createCourseSuccess(savedCourse));
     }).catch(error => {
       throw(error);
     });
