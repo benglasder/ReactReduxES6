5a27912f409d3024e5570122c400f3e27ac26dda
Ben Glasder <bglasder@gmail.com>
2018-04-14 00:21:51 -0500

    using tdd. added validation test

--- src/components/course/ManageCoursePage.test.js
+++ src/components/course/ManageCoursePage.test.js
@@ -15,6 +15,7 @@ describe ('Manage Course Page', () => {
     const saveButton = wrapper.find('input').last();
     expect(saveButton.prop('type')).toBe('submit');
     saveButton.simulate('click');
+    expect(wrapper.state().errors.title).toBe('Title must be at least 5 characters.');
   });
 });
 
