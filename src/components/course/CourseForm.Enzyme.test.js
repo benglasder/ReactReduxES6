8c7d75e9c237b1cb790312f78c18b349ee318562
Ben Glasder <bglasder@gmail.com>
2018-04-14 00:02:48 -0500

    wrapped enzyme tests in a describe

--- src/components/course/CourseForm.Enzyme.test.js
+++ src/components/course/CourseForm.Enzyme.test.js
@@ -14,18 +14,22 @@ function setup(saving) {
   return shallow(<CourseForm {...props}/>);
 }
 
-it('renders form and h1', () => {
-  const wrapper = setup(false);
-  expect(wrapper.find('form').length).toBe(1); // find a form, only be 1 form element
-  expect(wrapper.find('h1').text()).toEqual('Manage Course');
-});
+describe('CourseForm via Enzyme', () => {
 
-it('save button is labeled "Save" when not saving', () => {
-  const wrapper = setup(false);
-  expect(wrapper.find('input').props().value).toBe('Save');
-});
+  it('renders form and h1', () => {
+    const wrapper = setup(false);
+    expect(wrapper.find('form').length).toBe(1); // find a form, only be 1 form element
+    expect(wrapper.find('h1').text()).toEqual('Manage Course');
+  });
 
-it('save button is labeled "Saving..." when saving', () => {
-  const wrapper = setup(true);
-  expect(wrapper.find('input').props().value).toBe('Saving...');
+  it('save button is labeled "Save" when not saving', () => {
+    const wrapper = setup(false);
+    expect(wrapper.find('input').props().value).toBe('Save');
+  });
+
+  it('save button is labeled "Saving..." when saving', () => {
+    const wrapper = setup(true);
+    expect(wrapper.find('input').props().value).toBe('Saving...');
+  });
+
 });
